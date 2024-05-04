57a75e27e9d70b677d7592aadd9e28637ab09331
Carlos García Pérez <carlosziok@gmail.com>
2024-05-04 13:20:07 +0300

    Initial Commit

---
+++ MyContacts/MyContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyContactsApp.swift
+//  MyContacts
+//
+//  Created by Carlos Garcia Perez on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MyContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
